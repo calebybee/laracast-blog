02a48bf89837b0aa0dc860c79fb6c644a8e81e17
calebybee <calebybee@randallreilly.com>
2022-01-03 10:33:26 -0500

    adjusted views to pull from db

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,12 +2,12 @@
     @foreach ($posts as $post)
         <article>
             <h1>
-                <a href="/posts/{{ $post->slug }}">
+                <a href="/posts/{{ $post->id }}">
                     {{ $post->title }}
                 </a>
             </h1>
             <div>
-                {{ $post->excerpt }}
+                {{ $post->excerpt }}exi
             </div>
         </article>
     @endforeach
